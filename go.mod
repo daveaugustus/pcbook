c6594c6004b448547c43214f064196619d41b459
Dave Augustus <davetweetlive@gmail.com>
2021-07-10 23:25:35 +0530

    Added all random components

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/davetweetlive/pcbook
+
+go 1.16
+
+require (
+	github.com/golang/protobuf v1.5.2
+	github.com/google/uuid v1.2.0
+)
---
+++ sample/random.go
@@ -0,0 +1,124 @@
+package sample
+
+import (
+	"math/rand"
+
+	"github.com/davetweetlive/pcbook/pb"
+	"github.com/google/uuid"
+)
+
+func randomKeyboardLayout() pb.Keyboard_Layout {
+	switch rand.Intn(3) {
+	case 1:
+		return pb.Keyboard_QWERTY
+	case 2:
+		return pb.Keyboard_QWERTZ
+	default:
+		return pb.Keyboard_AZERTY
+	}
+}
+
+func randomCPUBrand() string {
+	return randomStringFromSet("Intel", "AMD")
+}
+
+func randomCPUName(brand string) string {
+	if brand == "Intel" {
+		return randomStringFromSet(
+			"Xeon E-2286M",
+			"Core i9-9980HK",
+			"Core i7-8750H",
+			"Core i5-9400F",
+			"Core i3-1005G1",
+		)
+	}
+
+	return randomStringFromSet(
+		"Ryzen 7 PRO 2700U",
+		"Ryzen 5 PRO 3500U",
+		"Ryzen 3 PRO 3200GE",
+	)
+}
+
+func randomGPUBrand() string {
+	return randomStringFromSet("NVIDIA", "AMD")
+}
+func randomGPUName(brand string) string {
+	if brand == "NVIDIA" {
+		return randomStringFromSet(
+			"RTX 2060",
+			"RTX 2070",
+			"GTX 1660-Ti",
+			"GTX 1070",
+		)
+	}
+	return randomStringFromSet(
+		"RX 590",
+		"RX 580",
+		"RX 5700-XT",
+		"RX Vega-56",
+	)
+}
+
+func randomLaptopBrand() string {
+	return randomStringFromSet("Apple", "Dell", "Lenovo")
+}
+
+func randomLaptopName(brand string) string {
+	switch brand {
+	case "Apple":
+		return randomStringFromSet("Macbook Air", "Macbook Pro")
+	case "Dell":
+		return randomStringFromSet("Latitude", "Vostro", "XPS", "Alienware")
+	default:
+		return randomStringFromSet("Thinkpad X1", "Thinkpsd P1", "Thinkpsd PS3")
+	}
+}
+
+func randomScreenResolution() *pb.Screen_Resolution {
+	height := randomInt(1080, 4320)
+	width := height * 16 / 9
+
+	resolution := &pb.Screen_Resolution{
+		Height: uint32(height),
+		Width:  uint32(width),
+	}
+
+	return resolution
+}
+func randomScreenPanel() pb.Screen_Panel {
+	if rand.Intn(2) == 1 {
+		return pb.Screen_IPS
+	}
+	return pb.Screen_OLED
+}
+
+func randomStringFromSet(a ...string) string {
+	n := len(a)
+	if n == 0 {
+		return ""
+	}
+	return a[rand.Intn(n)]
+}
+
+// rand.Intn generates value from 0 to 2. If thew generated value is 1 then it
+// returns true ealse it will return false.
+func randomBool() bool {
+	return rand.Intn(2) == 1
+}
+
+func randomInt(min, max int) int {
+	return min + rand.Intn(max-min+1)
+}
+
+func randomFloat64(min, max float64) float64 {
+	return min + rand.Float64()*(max-min)
+}
+
+func randomFloat32(min, max float32) float32 {
+	return min + rand.Float32()*(max-min)
+}
+
+func randomId() string {
+	return uuid.New().String()
+}
